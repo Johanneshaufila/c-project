61bd445054983e7995c8e50231355bc906eac214
Johannes Haufila <33969831+Johanneshaufila@users.noreply.github.com>
2022-01-12 18:04:26 +0200

    now

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Chapter2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please Enter Your AGE: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Name for John is: " + name);
+            Console.WriteLine("The age for John is: " + age);
+            Console.Read();
         }
     }
 }
